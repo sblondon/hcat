1d121ded14ba8633ce8f9e3b8b7c6113c5afe304
Stéphane Blondon <stephane.blondon@gmail.com>
2016-04-29 15:19:21 +0200

    fix lacking double quote

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
             "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 2.7",
-            "Topic :: Terminals,
+            "Topic :: Terminals",
         ],      
 
 
